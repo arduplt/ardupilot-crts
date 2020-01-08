923805a09a2cb8a5e1e5cfde15d9682a256ab1d8
arduplt <58990016+arduplt@users.noreply.github.com>
2020-01-08 03:11:44 +0300

    Communication via UART with an external micro
    Own simplified protocol

--- libraries/AP_Ticommunication/AP_Ticommunication.cpp
+++ libraries/AP_Ticommunication/AP_Ticommunication.cpp
@@ -25,319 +25,147 @@
 
 extern const AP_HAL::HAL& hal;
 
+/*
+AP_Ticommunication::AP_Ticommunication()
+{
+    //AP_Param::setup_object_defaults(this, var_info);
 
+    if (_singleton != nullptr) {
+        AP_HAL::panic("AP_Ticommunication must be singleton");
+    }
+    _singleton = this;
+}
+*/
 
 // Initialize backends based on existing params
 void AP_Ticommunication::init(void)
 {
 	// Search for the Port assigned to Ticommunication
-	port = AP::serialmanager().find_serial(AP_SerialManager::Serialprotocol_Ticommunication, 0);
+	port = AP::serialmanager().find_serial(AP_SerialManager::SerialProtocol_Ticommunication, 0);
         return;
 }
 
-void AP_Ticommunication_Serial::communicate_Ti()
+
+
+void AP_Ticommunication::update(void)
 {
     if (!port) {    // If port pointer points to NULL return
         return;
     }
 
-    uint32_t now = AP_HAL::millis();
+    // uint32_t now = AP_HAL::millis();
 	
     // read any available lines from the Ti
-    //    if character ">" is received it means we have received a full packet so send for processing
+    //    if character "<" is received it means we have received the beginning of a packet
     //    from HC to Pixhawk < 1, 74, 422> where 1 is the Opcode, 74 the state of charge %, 422 remaining flight time in minutes
-	
-    uint16_t count = 0;
-    int16_t nbytes = uart->available();
-    while (nbytes-- > 0) {
-		
-        char received_char = uart->read();
-		input_buffer[num_bytes_in_block_received] = received_char;
-        // check for end of packet
-        if (received_char == '>')    // End of Packet received, put packet in buffer
-		{
-			for (i=0, i<10, i++)
-			{
-				input_buffer
-			}
-		}
 
-    return (count > 0);
-	
+    int16_t nbytes = port->available();
+	bool message_start_found = false;
+    int buffer_count;
+	if (is_healthy()) {     // Send Heartbeat <1> if connection is Healthy 
+	port->write("<1>");
 	}
-
-
-    const uint32_t expected_bytes = 2 + (RT_LAST_OFFSET - RT_FIRST_OFFSET) + 4;
-    if (port->available() >= expected_bytes && read_incoming_realtime_data()) {
-        last_response_ms = now;
-        copy_to_frontend();
-    }
-
-    if (port->available() == 0 || now - last_response_ms > 200) {
-        // clear the input buffer
-        uint32_t buffered_data_size = port->available();
-        for (uint32_t i = 0; i < buffered_data_size; i++) {
-            port->read();
+    
+	if (nbytes >= 10) {
+    while (nbytes-- > 0) {                   // While Loop continues until all bytes in the Buffer are read
+        char c = port->read();
+        if (c == '<' ) {                     // Start of one message
+            buffer_count = 0;
+			message_start_found = true;
         }
-        // Request an update from the realtime table (7).
-        // The data we need start at offset 6 and ends at 129
-        send_request(7, RT_FIRST_OFFSET, RT_LAST_OFFSET);
-    }
-}
+        
+		if (message_start_found == false) continue;  // we did not find the start of the message yet, continue searching for it
+		
+		// This code only executes if the message Start "<" was found
+        buffer[buffer_count] = c;
+		buffer_count++;
+		
+		if (buffer_count==10) {
+			if (check_message ()) {
+			    // A valid message is in the buffer
+				last_updated_ms = AP_HAL::millis();
+				break;             // break the while loop
+			}
+			else {                                   // If the message is not valid, look for a new valid message
+				message_start_found = false;
+			    continue;	}
+		}
+	}
+	
+	}  // Endif nbytes >=10
 
-bool AP_Ticommunication::is_healthy(void) const      // Sonin Aero: Check Signal, if Healthy return TRUE and send <1> to Ticommunication
+	
+	
+	
+}
+	
+bool AP_Ticommunication::check_message()
 {
-	if (port && (AP_HAL::millis() - state.last_updated_ms) < HEALTHY_LAST_RECEIVED) {
-		port->write("<1>");
+	
+	
+	if (buffer[0] != '<' || buffer[2] != ',' || buffer[5] != ',' || buffer[9] != '>') return false;  // Check message structure < x,xx,xxx>
+	
+	int number_index[]={1,3,4,6,7,8};
+	
+	for (int i=0; i <= 5 ; i++)
+	{
+		if ( buffer[number_index[i]] > '9' || buffer[number_index[i]] < '0' ) return false;
+		else continue; // check if x xx xxx are numbers from 0-9
+	}
+	
+	
+	return true;    // if everything is ok message is valid
+
+}
+		
+
+
+bool AP_Ticommunication::is_healthy()       // Sonin Aero: Check Signal, if Healthy return TRUE and send <1> to Ticommunication
+{
+	if (port && (AP_HAL::millis() - last_updated_ms) < 3000) {     // check if last Update is less than 3000 ms ago
 		return true;
 	}
     else return false;    
 }
 
 
-/*
-  send Ticommunication_STATUS
- */
- 
-/*
-void AP_Ticommunication::send_mavlink_status(mavlink_channel_t chan)
-{
-    if (!backend) {
-        return;
-    }
-    mavlink_msg_Ticommunication_status_send(
-        chan,
-        AP_Ticommunication::is_healthy(),
-        state.ecu_index,
-        state.engine_speed_rpm,
-        state.estimated_consumed_fuel_volume_cm3,
-        state.fuel_consumption_rate_cm3pm,
-        state.engine_load_percent,
-        state.throttle_position_percent,
-        state.spark_dwell_time_ms,
-        state.atmospheric_pressure_kpa,
-        state.intake_manifold_pressure_kpa,
-        (state.intake_manifold_temperature - 273.0f),
-        (state.cylinder_status[0].cylinder_head_temperature - 273.0f),
-        state.cylinder_status[0].ignition_timing_deg,
-        state.cylinder_status[0].injection_time_ms);
-}
-*/
 
+int  AP_Ticommunication::get_soc() {
+	int soc;
+	char soc_buffer [2];
+	soc_buffer[0]= buffer [3];
+	soc_buffer[1]= buffer [4];
+	
+	soc = atoi(soc_buffer);
+	
+	return soc;	
+}
+
+int AP_Ticommunication::get_remaining_flight_time () {
+	
+	int remaining_flight_time;
+	char rft_buffer [3];
+	rft_buffer[0]= buffer [6];
+	rft_buffer[1]= buffer [7];
+	rft_buffer[1]= buffer [8];
+	
+	remaining_flight_time = atoi(rft_buffer);
+	
+	return remaining_flight_time;	
+}
+
+/*
 namespace AP {
 AP_Ticommunication *Ticommunication()
 {
     return AP_Ticommunication::get_singleton();
 }
 }
-
+*/
 extern const AP_HAL::HAL &hal;
 
 
-
-void AP_Ticommunication_Backend::copy_to_frontend() 
-{
-    WITH_SEMAPHORE(sem);
-    frontend.state = internal_state;
-}
-
-float AP_Ticommunication_Backend::get_coef1(void) const
-{
-    return frontend.coef1;
-}
-
-float AP_Ticommunication_Backend::get_coef2(void) const
-{
-    return frontend.coef2;
-}
-
- 
-
-
-
-
-bool AP_Ticommunication_Serial::read_incoming_realtime_data() 
-{
-    // Data is parsed directly from the buffer, otherwise we would need to allocate
-    // several hundred bytes for the entire realtime data table or request every
-    // value individiually
-    uint16_t message_length = 0;
-
-    // reset checksum before reading new data
-    checksum = 0;
-    
-    // Message length field begins the message (16 bits, excluded from CRC calculation)
-    // Message length value excludes the message length and CRC bytes 
-    message_length = port->read() << 8;
-    message_length += port->read();
-
-    if (message_length >= 256) {
-        // don't process invalid messages
-        // hal.console->printf("message_length: %u\n", message_length);
-        return false;
-    }
-
-    // Response Flag (see "response_codes" enum)
-    response_flag = read_byte_CRC32();
-    if (response_flag != RESPONSE_WRITE_OK) {
-        // abort read if we did not receive the correct response code;
-        return false;
-    }
-    
-    // Iterate over the payload bytes 
-    for ( uint8_t offset=RT_FIRST_OFFSET; offset < (RT_FIRST_OFFSET + message_length - 1); offset++) {
-        uint8_t data = read_byte_CRC32();
-        float temp_float;
-        switch (offset) {
-            case PW1B:
-                internal_state.cylinder_status[0].injection_time_ms = (float)((data << 8) + read_byte_CRC32())/1000.0f;
-                offset++;  // increment the counter because we read a byte in the previous line
-                break;
-            case RPMB:
-                // Read 16 bit RPM
-                internal_state.engine_speed_rpm = (data << 8) + read_byte_CRC32();
-                offset++;
-                break;
-            case ADVANCEB:
-                internal_state.cylinder_status[0].ignition_timing_deg = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                break;
-            case ENGINE_BM:
-                break;
-            case BAROMETERB:
-                internal_state.atmospheric_pressure_kpa = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                break;
-            case MAPB:
-                internal_state.intake_manifold_pressure_kpa = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                break;
-            case MATB:
-                temp_float = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                internal_state.intake_manifold_temperature = f_to_k(temp_float);
-                break;
-            case CHTB:
-                temp_float = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                internal_state.cylinder_status[0].cylinder_head_temperature = f_to_k(temp_float);
-                break;
-            case TPSB:
-                temp_float = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                internal_state.throttle_position_percent = roundf(temp_float);
-                break;
-            case AFR1B:
-                temp_float = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                offset++;
-                internal_state.cylinder_status[0].lambda_coefficient = temp_float;
-                break;
-            case DWELLB:
-                temp_float = (float)((data << 8) + read_byte_CRC32())/10.0f;
-                internal_state.spark_dwell_time_ms = temp_float;
-                offset++;
-                break;
-            case LOAD:
-                internal_state.engine_load_percent = data;
-                break;
-            case FUEL_PRESSUREB:
-                // MS Fuel Pressure is unitless, store as KPA anyway
-                temp_float = (float)((data << 8) + read_byte_CRC32());
-                internal_state.fuel_pressure = temp_float;
-                offset++;
-                break;   
-                
-        }
-    }
-    
-    // Read the four CRC bytes
-    uint32_t received_CRC;
-    received_CRC = port->read() << 24;
-    received_CRC += port->read() << 16;
-    received_CRC += port->read() << 8;
-    received_CRC += port->read();
-                        
-    if (received_CRC != checksum) {
-        // hal.console->printf("Ticommunication CRC: 0x%08x 0x%08x\n", received_CRC, checksum);
-        return false;
-    }
-
-    // Calculate Fuel Consumption 
-    // Duty Cycle (Percent, because that's how HFE gives us the calibration coefficients)
-    float duty_cycle = (internal_state.cylinder_status[0].injection_time_ms * internal_state.engine_speed_rpm)/600.0f;
-    uint32_t current_time = AP_HAL::millis();
-    // Super Simplified integration method - Error Analysis TBD
-    // This calcualtion gives erroneous results when the engine isn't running
-    if (internal_state.engine_speed_rpm > RPM_THRESHOLD) {
-        internal_state.fuel_consumption_rate_cm3pm = duty_cycle*get_coef1() - get_coef2();
-        internal_state.estimated_consumed_fuel_volume_cm3 += internal_state.fuel_consumption_rate_cm3pm * (current_time - internal_state.last_updated_ms)/60000.0f;
-    } else {
-        internal_state.fuel_consumption_rate_cm3pm = 0;
-    }
-    internal_state.last_updated_ms = current_time;
-    
-    return true;
-         
-}
-
-void AP_Ticommunication_Serial::send_request(uint8_t table, uint16_t first_offset, uint16_t last_offset)
-{
-    uint16_t length = last_offset - first_offset + 1;
-    // Fixed message size (0x0007)
-    // Command 'r' (0x72)
-    // Null CANid (0x00)
-    const uint8_t data[9] = {
-        0x00,
-        0x07,
-        0x72,
-        0x00,
-        (uint8_t)table,
-        (uint8_t)(first_offset >> 8),
-        (uint8_t)(first_offset),
-        (uint8_t)(length >> 8),
-        (uint8_t)(length)   
-    };
-    
-    uint32_t crc = 0;
-    
-    // Write the request and calc CRC
-    for (uint8_t i = 0;  i != sizeof(data) ; i++) {
-        // Message size is excluded from CRC
-        if (i > 1) {
-            crc = CRC32_compute_byte(crc, data[i]);
-        }
-        port->write(data[i]);
-    }
-    
-    // Write the CRC32
-    port->write((uint8_t)(crc >> 24));
-    port->write((uint8_t)(crc >> 16));
-    port->write((uint8_t)(crc >> 8));
-    port->write((uint8_t)crc);
-
-}
-
-uint8_t AP_Ticommunication_Serial::read_byte_CRC32()
-{   
-    // Read a byte and update the CRC 
-    uint8_t data = port->read();
-    checksum = CRC32_compute_byte(checksum, data);
-    return data;
-}
-
-// CRC32 matching MegaSquirt
-uint32_t AP_Ticommunication_Serial::CRC32_compute_byte(uint32_t crc, uint8_t data)
-{
-    crc ^= ~0U;
-    crc = crc_crc32(crc, &data, 1);
-    crc ^= ~0U;
-    return crc;
-}
-
-
-
-
-#endif // Ticommunication_ENABLED
+//#endif // Ticommunication_ENABLED
 
 
 
--- libraries/AP_Ticommunication/AP_Ticommunication.h
+++ libraries/AP_Ticommunication/AP_Ticommunication.h
@@ -21,15 +21,11 @@
 #include <AP_Common/AP_Common.h>
 #include <AP_Param/AP_Param.h>
 #include <GCS_MAVLink/GCS.h>
-
 #include <AP_HAL/AP_HAL.h>
-
-// Time in milliseconds before we declare the Ticommunication to be "unhealthy"
-#Ticommunicationne HEALTHY_LAST_RECEIVED 3000
-
+#include <AP_Math/AP_Math.h>
 
 /*
-*This library is for communication with the Texas Insttruments microcontroller, connected via Serial
+*This library is for communication with the Texas Instruments microcontroller, connected via Serial
 * 
 *
  */
@@ -37,13 +33,17 @@
 class AP_Ticommunication {
 public:
 
+
+	//AP_Ticommunication();  // Constructor
     // Initializes backend  
     void init(void);          // chkd
 
     // Requests backend to update the frontend. Should be called at 10Hz.
-    void communicate_Ti();   // main loop function Sonin Aero ************************************************
+    void update(void);   // main loop function Sonin Aero ************************************************
 	
-	char input_buffer[10];   // used to store incoming values
+	
+	
+	
 	
 	//  check if message is valid
 	
@@ -60,235 +60,25 @@ public:
 	
 	void send_heartbeat();
 
-    // returns enabled state of Ticommunication
-    bool enabled() const { return type != Ticommunication_COMMUNICATION_TYPE_NONE; }
 
-    bool is_healthy() const;
+    bool is_healthy();
 
-protected:
-
-
-    Ticommunication_State state;
 
 private:
     // port
     AP_HAL::UARTDriver *port;
     // Front End Parameters
-    AP_Int8 type;
+    //AP_Int8 type;
+	
+	char buffer[10];   // used to store incoming values
+	
+	uint32_t last_updated_ms;
 
     // Tracking backends
-    AP_Ticommunication_Backend *backend;
+    
  
 };
 
-
-
-class AP_Ticommunication_Backend {
-public:    
-    // Constructor with initialization
-    AP_Ticommunication_Backend(AP_Ticommunication &_frontend);
-
-    // Virtual destructor that Ticommunication backends can override 
-    virtual ~AP_Ticommunication_Backend(void) {}
-
-    // Update the state structure
-    virtual void update() = 0;
-
-protected:
-    // Copies internal state to the frontend state
-    void copy_to_frontend();
-
-    // Semaphore for access to shared frontend data
-    HAL_Semaphore sem;
-
-    // Internal state for this driver (before copying to frontend)
-    Ticommunication_State internal_state;
-
-    int8_t get_uavcan_node_id(void) const;
-    float get_coef1(void) const;
-    float get_coef2(void) const;
-
-private:
-    AP_Ticommunication &frontend;
-};
-
-
-class AP_Ticommunication_Serial: public AP_Ticommunication_Backend {
-    
-public:
-    // Constructor with initialization
-    AP_Ticommunication_Serial(AP_Ticommunication &_frontend);
-
-    // Update the state structure
-    void update() override;
-
-private:
-    AP_HAL::UARTDriver *port;
-    void parse_realtime_data();
-    bool read_incoming_realtime_data();
-    void send_request(uint8_t table, uint16_t first_offset, uint16_t last_offset);
-    uint8_t read_byte_CRC32();
-    uint32_t CRC32_compute_byte(uint32_t inCrc32, uint8_t data);
-    float f_to_k(float temp_f) { return (temp_f + 459.67f) * 0.55556f; };
-    
-    // Serial protocol Variables
-    uint32_t checksum;
-    uint8_t step;
-    uint8_t response_flag;
-    uint16_t message_counter;
-    uint32_t last_response_ms;
-
-    // confirmed that last command was ok
-    bool last_command_confirmed;
-
-    // Command Response Codes
-  /*  enum response_codes {
-        RESPONSE_WRITE_OK =0x00,
-        RESPONSE_REALTIME_DATA,
-        RESPONSE_PAGE_DATA,
-        RESPONSE_CONFIG_ERROR,
-        RESPONSE_PAGE10_OK,
-        RESPONSE_CAN_DATA,
-        // Error Responses
-        ERR_UNDERRUN = 0X80,
-        ERR_OVERRUN,
-        ERR_CRC_FAILURE,
-        ERR_UNRECOGNIZED_COMMAND,
-        ERR_OUT_OF_RANGE,
-        ERR_SERIAL_BUSY,
-        ERR_FLASH_LOCKED,
-        ERR_SEQ_FAIL_1,
-        ERR_SEQ_FAIL_2,
-        ERR_CAN_QUEUE_FULL,
-        ERR_CAN_TIMEOUT,
-        ERR_CAN_FAILURE,
-        ERR_PARITY,
-        ERR_FRAMING,
-        ERR_SERIAL_NOISE,
-        ERR_TXMODE_RANGE,
-        ERR_UNKNOWN
-    };*/
-    
-    // Realtime Data Table Locations
-  /*  enum realtime_data {
-        PW1B = 2,
-        PW1_LSB,
-        RPMB = 6,
-        RPM_LSB,
-        ADVANCEB,
-        ADVANCE_LSB,
-        ENGINE_BM = 11,
-        BAROMETERB = 16,
-        BAROMETER_LSB,
-        MAPB,
-        MAP_LSB,
-        MATB,
-        MAT_LSB,
-        CHTB,
-        CHT_LSB,
-        TPSB,
-        TPS_LSB,
-        AFR1B = 28,
-        AFR1_LSB,
-        AFR2B,
-        AFR2_LSB,
-        DWELLB = 62,
-        DWELL_LSB,
-        LOAD = 66,
-        FUEL_PRESSUREB = 128,
-        FUEL_PRESSURE_LSB, 
-        // Helpers used when sending request
-        RT_FIRST_OFFSET = PW1B,
-        RT_LAST_OFFSET = FUEL_PRESSURE_LSB
-    };*/
-};
-
-
-/***************
- *
- * Status enums
- *
- ***************/
-
-
-// Stores the current state read by the Ticommunication system
-// All backends are required to fill in this state structure
-struct Ticommunication_State {
-    // When this structure was last updated (milliseconds)
-    uint32_t last_updated_ms;
-
-    // Current overall engine state
-    Engine_State engine_state;
- 
-    // If there is an error that does not fit other error types
-    bool general_error;
-
-    // Error/status fields 
-    Crankshaft_Sensor_Status crankshaft_sensor_status;
-    Temperature_Status temperature_status;
-    Fuel_Pressure_Status fuel_pressure_status;
-    Oil_Pressure_Status oil_pressure_status;
-    Detonation_Status detonation_status;
-    Misfire_Status misfire_status;
-    Debris_Status debris_status;
-
-    // Engine load (percent)
-    uint8_t engine_load_percent;
-    
-    // Engine speed (revolutions per minute)
-    uint32_t engine_speed_rpm;
-
-    // Spark dwell time (milliseconds)
-    float spark_dwell_time_ms;
-
-    // Atmospheric (barometric) pressure (kilopascal)
-    float atmospheric_pressure_kpa;
-
-    // Engine intake manifold pressure (kilopascal)
-    float intake_manifold_pressure_kpa;
-
-    // Engine intake manifold temperature (kelvin)
-    float intake_manifold_temperature;
-
-    // Engine coolant temperature (kelvin)
-    float coolant_temperature;
-    
-    // Oil pressure (kilopascal)
-    float oil_pressure;
-
-    // Oil temperature (kelvin)
-    float oil_temperature;
-
-    // Fuel pressure (kilopascal)
-    float fuel_pressure;
-
-    // Instant fuel consumption estimate, which 
-    // should be low-pass filtered in order to prevent aliasing effects.
-    // (centimeter^3)/minute.
-    float fuel_consumption_rate_cm3pm;
-
-    // Estimate of the consumed fuel since the start of the engine (centimeter^3)
-    // This variable is reset when the engine is stopped.
-    float estimated_consumed_fuel_volume_cm3;
-
-    // Throttle position (percent)
-    uint8_t throttle_position_percent;
-
-    // The index of the publishing ECU.
-    uint8_t ecu_index;
-
-    // Spark plug activity report.
-    // Can be used during pre-flight tests of the spark subsystem.
-    // Use case is that usually on double spark plug engines, the 
-    // engine switch has the positions OFF-LEFT-RIGHT-BOTH-START.
-    // Gives pilots the possibility to test both spark plugs on 
-    // ground before takeoff.
-    Spark_Plug_Usage spark_plug_usage;
-
-    // Status for each cylinder in the engine
-    Cylinder_Status cylinder_status[ENGINE_MAX_CYLINDERS];
-
-};
 
 
 namespace AP {
@@ -299,95 +89,3 @@ namespace AP {
  
  //**********************************************************************DELETE******************************************************************
  
- enum class Engine_State : uint8_t {
-    STOPPED  = 0,
-    STARTING = 1,
-    RUNNING  = 2,
-    FAULT    = 3
-};
-
-enum class Crankshaft_Sensor_Status : uint8_t {
-    NOT_SUPPORTED = 0,
-    OK            = 1,
-    ERROR         = 2
-};
-
-enum class Temperature_Status : uint8_t {
-    NOT_SUPPORTED       = 0,
-    OK                  = 1,
-    BELOW_NOMINAL       = 2,
-    ABOVE_NOMINAL       = 3,
-    OVERHEATING         = 4,
-    EGT_ABOVE_NOMINAL   = 5
-};
-
-enum class Fuel_Pressure_Status : uint8_t {
-    NOT_SUPPORTED        = 0,
-    OK                   = 1,
-    BELOW_NOMINAL        = 2,
-    ABOVE_NOMINAL        = 3
-};
-
-enum class Oil_Pressure_Status : uint8_t {
-    OIL_PRESSURE_STATUS_NOT_SUPPORTED = 0,
-    OIL_PRESSURE_OK                   = 1,
-    OIL_PRESSURE_BELOW_NOMINAL        = 2,
-    OIL_PRESSURE_ABOVE_NOMINAL        = 3
-};
-
-enum class Detonation_Status : uint8_t {
-    NOT_SUPPORTED  = 0,
-    NOT_OBSERVED   = 1,
-    OBSERVED       = 2
-};
-
-enum class Misfire_Status : uint8_t {
-    NOT_SUPPORTED = 0,
-    NOT_OBSERVED  = 1,
-    OBSERVED      = 2
-};
-
-enum class Debris_Status : uint8_t {
-    NOT_SUPPORTED = 0,
-    NOT_DETECTED  = 1,
-    DETECTED      = 2
-};
-
-enum class Spark_Plug_Usage : uint8_t {
-    SINGLE        = 0,
-    FIRST_ACTIVE  = 1,
-    SECOND_ACTIVE = 2,
-    BOTH_ACTIVE   = 3
-};
-
-
-/***************
- * Status structs.
- * Ticommunications may not provide all data in the message, therefore, the following guidelines should be followed.
- * All integer fields are required unless stated otherwise.
- * All floating point fields are optional unless stated otherwise; unknown/unapplicable fields will be NaN.
- ***************/
-
-
-// Per-cylinder status struct
-struct Cylinder_Status {
-    // Cylinder ignition timing (angular degrees of the crankshaft)
-    float ignition_timing_deg;
-
-    // Fuel injection time (millisecond)
-    float injection_time_ms;
-
-    // Cylinder head temperature (CHT) (kelvin)
-    float cylinder_head_temperature;
-
-    // Exhaust gas temperature (EGT) (kelvin)
-    // If this cylinder is not equipped with an EGT sensor - will be NaN
-    // If there is a single shared EGT sensor, will be the same value for all cylinders
-    float exhaust_gas_temperature;
-
-    // Estimated lambda coefficient (dimensionless ratio)
-    // Useful for monitoring and tuning purposes.
-    float lambda_coefficient;
-};
-
-
